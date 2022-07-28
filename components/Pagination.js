51afb9f58e6a77959785c15962c9d152562c2235
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-07-28 18:29:00 +0200

    added basic css to Pagination btns, needs more work

--- components/Pagination.js
+++ components/Pagination.js
@@ -3,13 +3,19 @@ import React from 'react'
 export function Pagination({ nButtons, setSkip }) {
 
   const totalButtons = Math.floor(nButtons/9)
+
+  const paginationStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: "center",
+  }
   
   return (
-    <>
+    <div style={paginationStyle}>
       {[...Array(totalButtons)].map((e, i) => {
         return <button key={i+1} onClick={() => setSkip(i+1)}> {i+1}</button>
       })}
           
-    </>
+    </div>
   )
 }
